c0b8f60a35ca2f6b8eee49c7adf3f9b90184e072
Prod23 <f20202075@hyderabad.bits-pilani.ac.in>
2022-07-17 09:26:14 +0530

    adding minutes

--- frontPage.js
+++ frontPage.js
@@ -1,6 +1,8 @@
 let seconds = 00;
 let tens = 00;
+let mins = 00;
 let appendTens = document.getElementById("tens");
+let appendMins = document.getElementById("mins");
 let appendSeconds = document.getElementById("seconds");
 let buttonStart = document.getElementById("start");
 let buttonStop = document.getElementById("stop");
@@ -22,8 +24,17 @@ function startTimer(){
     tens = 0;
     appendTens.innerHTML = "0"+0;
   }
+  if(seconds > 59){
+    mins++;
+    appendMins.innerHTML = "0"+mins;
+    seconds = 0;
+    appendSeconds.innerHTML = "0"+seconds;
+  }
   if (seconds > 9){
     appendSeconds.innerHTML = seconds;
+  }
+  if (mins > 9){
+    appendMins.innerHTML = mins;
   }
 }
 function start(){
